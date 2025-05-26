922f2e464616a23a25156e000f6d89fe3f304691
DelannoyArthur <delannoyarthur@outlook.com>
2025-05-26 17:35:39 +0200

    personalities

--- js/test.js
+++ js/test.js
@@ -7,33 +7,33 @@ function findPersonality() {
         result.getElementsByTagName("h3")[0].textContent = "You are a white sheet of paper.";
         console.log("you are a white sheet of paper");
     } else if (score[0] === score[1] && score[1] === score[2]) {
-        result.getElementsByTagName("img")[0].src = "origami/totoro/totoro.gif";
-        result.getElementsByTagName("h3")[0].textContent = "You are a cameleon.";
-        console.log("you are a cameleon");
+        result.getElementsByTagName("img")[0].src = "origami/chameleon/chameleon.gif";
+        result.getElementsByTagName("h3")[0].textContent = "You are a chameleon.";
+        console.log("you are a chameleon");
     } else if (score[0] > score[1] && score[0] > score[2]) {
         result.getElementsByTagName("img")[0].src = "origami/totoro/totoro.gif";
         result.getElementsByTagName("h3")[0].textContent = "You are a totoro.";
         console.log("you are a totoro");
     } else if (score[1] > score[0] && score[1] > score[2]) {
-        result.getElementsByTagName("img")[0].src = "origami/totoro/totoro.gif";
+        result.getElementsByTagName("img")[0].src = "origami/turtle/turtle.gif";
+        result.getElementsByTagName("h3")[0].textContent = "You are a turtle.";
+        console.log("you are a turtle");
+    } else if (score[2] > score[0] && score[2] > score[1]) {
+        result.getElementsByTagName("img")[0].src = "origami/the_frog_prince/the_frog_prince.gif";
+        result.getElementsByTagName("h3")[0].textContent = "You are the frog prince.";
+        console.log("you are the frog prince");
+    } else if (score[0] === score[1] && score[0] > score[2]) {
+        result.getElementsByTagName("img")[0].src = "origami/elephant/elephant.gif";
+        result.getElementsByTagName("h3")[0].textContent = "You are an elephant.";
+        console.log("you are an elephant");
+    } else if (score[0] === score[2] && score[0] > score[1]) {
+        result.getElementsByTagName("img")[0].src = "origami/cat/cat.gif";
         result.getElementsByTagName("h3")[0].textContent = "You are a cat.";
         console.log("you are a cat");
-    } else if (score[2] > score[0] && score[2] > score[1]) {
-        result.getElementsByTagName("img")[0].src = "origami/totoro/totoro.gif";
-        result.getElementsByTagName("h3")[0].textContent = "You are a elephant.";
-        console.log("you are a elephant");
-    } else if (score[0] === score[1] && score[0] > score[2]) {
-        result.getElementsByTagName("img")[0].src = "origami/totoro/totoro.gif";
-        result.getElementsByTagName("h3")[0].textContent = "You are a bear.";
-        console.log("you are a bear");
-    } else if (score[0] === score[2] && score[0] > score[1]) {
-        result.getElementsByTagName("img")[0].src = "origami/totoro/totoro.gif";
-        result.getElementsByTagName("h3")[0].textContent = "You are a ac.";
-        console.log("you are a ac");
     } else if (score[1] === score[2] && score[1] > score[0]) {
-        result.getElementsByTagName("img")[0].src = "origami/totoro/totoro.gif";
-        result.getElementsByTagName("h3")[0].textContent = "You are a bc.";
-        console.log("you are a bc");
+        result.getElementsByTagName("img")[0].src = "origami/athletic_bear/athletic_bear.gif";
+        result.getElementsByTagName("h3")[0].textContent = "You are an athletic bear.";
+        console.log("you are an athletic bear");
     }
 }
 
